02894341141dd9800503117f48094037e1791edf
mdrplg <mdrplg@gmail.com>
2014-10-11 17:17:46 -0700

    A simple form

--- Scripts/angular/home.js
+++ Scripts/angular/home.js
@@ -1,3 +1,7 @@
 ﻿angular.module("homeApp", []).controller("homeController", ['$scope', function ($scope) {
-    $scope.Name = 'Jeffrey Charles Bryant';
+    $scope.person = { name: 'Jeffrey Charles Bryant', address: '6229 SW Erickson', city: 'Beaverton', state: 'Oregon', zip: '97008', phones: { home: '503-430-5984', cell: '971-645-6207' } };
+    $scope.states = [
+        'Alaska', 'Alabama', 'Arizona', 'California', 'Connecticut', 'Kansas','Ohio','Oregon','Washington'
+    ];
+    $scope.style = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve'];
 }]);
